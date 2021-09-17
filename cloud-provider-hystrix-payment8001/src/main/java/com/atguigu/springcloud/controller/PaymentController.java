6a178c1ad6aadb6f18a477fe915a4123b7f1e74d
zhq <2847492481@qq.com>
2021-09-17 21:53:31 +0800

    third commit

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,37 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/hystrix/ok/{id}")
+    public String  paymentInfo_OK(Integer id){
+
+        String result = paymentService.paymentInfo_OK(id);
+        log.info("*******result:"+result);
+        return result;
+    }
+
+    @GetMapping(value = "/payment/hystrix/timeout/{id}")
+    public String  paymentInfo_TimeOut(Integer id){
+
+        String result = paymentService.paymentInfo_TimeOut(id);
+        log.info("*******result:"+result);
+        return result;
+    }
+
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.service;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+
+    public String paymentInfo_OK(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"paymentInfo_OK"+id;
+    }
+
+    public String paymentInfo_TimeOut(Integer id){
+
+        int timeNumber = 3;
+        try {
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池："+Thread.currentThread().getName()+"paymentInfo_TimeOut"+id;
+    }
+
+}
